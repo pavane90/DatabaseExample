41c33f21b3058484f46852af308fa49281340935
Lee, Byungman <pavane90@gmail.com>
2019-05-22 17:22:33 +0900

    6-4

--- app.js
+++ app.js
@@ -49,9 +49,67 @@ app.use(
 
 var router = express.Router();
 
+router.route('/process/login').post(function(req, res){
+  console.log('/process/login 라우팅 함수 호출됨.');
+
+  var paramId = req.body.id || req.query.id;
+  var paramPassword = req.body.password || req.query.password;
+  console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+
+  if (database) {
+    authUser(database, paramId, paramPassword, function(err, docs){
+      if(err) {
+        console.log('에러가 발생했습니다' + err);
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+        res.write('<h1>에러 발송</h1>');
+        res.end();
+        return;
+      }
+      if(docs) {
+        console.dir(docs);
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+        res.write('<h1>로그인 성공!</h1>');
+        res.write('<div><p>사용자 : ' + docs[0].name + '</p></div>');
+        res.end();
+        res.write('<br><br><a href="/public/login.html">다시 로그인하기</a>');
+      } else {
+        console.log('대상 데이터가 없음' + err);
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+        res.write('<h1>존재하지 않는 사용자입니다.</h1>');
+        res.end();
+      }
+    });
+  } else {
+    console.log('데이터베이스 연결에러' + err);
+    res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+    res.write('<h1>데이터베이스 연결에러</h1>');
+    res.end();
+  }
+
+});
+
 app.use("/", router);
 
-var authUser = function(database) {};
+var authUser = function(db, id, password, callback) {
+  console.log('authuser 호출됨' + id + ', ' + password);
+
+  var users = db.collection('users');
+
+  users.find({"id":id,"password":password}).toArray(function(err, docs){
+    if (err){
+      callback(err, null);
+      return;
+    } 
+
+    if (docs.length > 0) {
+      console.log('일치하는 사용자를 찾음');
+      callback(null, docs);
+    } else {
+      console.log('일치하는 사용자를 찾지못함');
+      callback(null, null);
+    }
+  });
+};
 
 var errorHandler = expressErrorHandler({
   ststic: {
