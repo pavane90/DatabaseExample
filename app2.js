5da03a747b4c16b4ae86cf8d5f6fcbabc0391a07
Lee, Byungman <pavane90@gmail.com>
2019-06-01 00:29:48 +0900

    mongodb v2 / ch06-05

--- app2.js
+++ app2.js
@@ -70,12 +70,53 @@ router.route("/process/login").post(function(req, res) {
         res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
         res.write("<h1>로그인 성공!</h1>");
         res.write("<div><p>사용자 : " + docs[0].name + "</p></div>");
-        res.end();
         res.write('<br><br><a href="/public/login.html">다시 로그인하기</a>');
+        res.end();
       } else {
         console.log("대상 데이터가 없음" + err);
         res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
         res.write("<h1>존재하지 않는 사용자입니다.</h1>");
+        res.end();
+      }
+    });
+  } else {
+    console.log("데이터베이스 연결에러" + err);
+    res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+    res.write("<h1>데이터베이스 연결에러</h1>");
+    res.end();
+  }
+});
+
+router.route("/process/adduser").post(function(req, res) {
+  console.log("/process/adduser 라우팅 호출");
+
+  var paramId = req.body.id || req.query.id;
+  var paramPassword = req.body.password || req.query.password;
+  var paramName = req.body.name || req.query.name;
+
+  console.log(
+    "요청 파라미터 : " + paramId + ", " + paramPassword + ", " + paramName
+  );
+
+  if (database) {
+    addUser(database, paramId, paramPassword, paramName, function(err, result) {
+      if (err) {
+        console.log("에러가 발생했습니다" + err);
+        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+        res.write("<h1>에러 발송</h1>");
+        res.end();
+        return;
+      }
+      if (result) {
+        console.dir(result);
+        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+        res.write("<h1>등록 완료!</h1>");
+        res.write("<div><p>사용자 : " + paramName + "</p></div>");
+        res.end();
+      } else {
+        console.log("에러 발생" + err);
+        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+        res.write("<h1>사용자 추가에 실패했습니다.</h1>");
         res.end();
       }
     });
@@ -119,7 +160,7 @@ var addUser = function(database, id, password, name, callback) {
     err,
     result
   ) {
-    if (eff) {
+    if (err) {
       callback(err, null);
       return;
     }
